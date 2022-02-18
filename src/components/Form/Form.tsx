6acd06bebd8de20bb3cfa50cd0b2d681be4945e9
adib sadeghi <adibsadeghi7272@gmail.com>
2022-02-18 21:51:37 +0330

    form and its validation was completed

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { HeaderContainer, StyledH } from "./Form.styled";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import * as yup from "yup";
+import { Typography } from "@material-ui/core";
 
 type Props = {};
 
 type FormInputs = {
   email: string;
   password: string;
+  save: boolean;
 };
 
 const schema = yup.object().shape({
@@ -21,6 +25,7 @@ const schema = yup.object().shape({
     .string()
     .min(6, "حداقل باید شش کاراکتر باشد")
     .required("وارد کردن پسورد ضروری است"),
+  save: yup.boolean(),
 });
 
 const Form = (props: Props) => {
@@ -29,16 +34,23 @@ const Form = (props: Props) => {
     handleSubmit,
     watch,
     control,
+    getValues,
     formState: { errors },
   } = useForm<FormInputs>({
     resolver: yupResolver(schema),
+    defaultValues:{
+      email:"",
+      password:"",
+      save:true
+    }
   });
 
   const formSubmitHandler: SubmitHandler<FormInputs> = (data: FormInputs) => {
     console.log(data);
   };
-
-  console.log(watch("email"))
+const values=getValues()
+console.log(values)
+  console.log(watch("email"));
   return (
     <>
       <HeaderContainer>
@@ -48,13 +60,14 @@ const Form = (props: Props) => {
         <Controller
           name="email"
           control={control}
-          defaultValue=""
+          defaultValue={values.email}
           render={({ field }) => (
             <TextField
               variant="standard"
-             
+              
               {...field}
               fullWidth
+              value={values.email}
               label="نام کاربری/ایمیل"
               error={!!errors.email}
               helperText={errors.email ? errors.email?.message : ""}
@@ -64,18 +77,46 @@ const Form = (props: Props) => {
         <Controller
           name="password"
           control={control}
-          defaultValue=""
+          defaultValue={values.password}
           render={({ field }) => (
             <TextField
               variant="standard"
               {...field}
               fullWidth
               label="پسورد"
+              value={values.password}
               error={!!errors.password}
               helperText={errors.password ? errors.password?.message : ""}
             />
           )}
         />
+
+        <div>
+        <FormControlLabel
+        sx={{marginRight:0}}
+       
+          control={
+            <Controller
+              control={control}
+              name="save"
+              defaultValue={true}
+              render={({ field: { onChange } }) => (
+                <Checkbox
+                  color="primary"
+                checked={values.save}
+                 sx={{p:0}}
+                  onChange={(e) => onChange(e.target.checked)}
+                />
+              )}
+            />
+          }
+          label={
+            <p>
+              مرا بخاطر بسپار
+            </p>
+          }
+        />
+        </div>
 
         <button type="submit">ارسال</button>
       </form>
