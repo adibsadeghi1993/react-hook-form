ce19cc106257ccb6e65989daddd1f3a690e30f8c
adib sadeghi <adibsadeghi7272@gmail.com>
2022-02-18 20:13:53 +0330

    schema for validation was created

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
-
+import { yupResolver } from "@hookform/resolvers";
 import { HeaderContainer, StyledH } from "./Form.styled";
 import { useForm, SubmitHandler } from "react-hook-form";
+import * as yup from "yup";
 
 type Props = {};
 
@@ -11,17 +12,30 @@ type FormInputs = {
   password: string;
 };
 
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("لطفا ایمیل صحیح وارد کنید")
+    .required("وارد کردن ایمیل ضروری است"),
+    password: yup
+    .string()
+    .min(6,"حداقل باید شش کاراکتر باشد")
+    .required("وارد کردن پسورد ضروری است"),
+});
+
 const Form = (props: Props) => {
   const {
     register,
     handleSubmit,
     watch,
     formState: { error },
-  } = useForm<FormInputs>();
+  } = useForm<FormInputs>({
+    resolver:yupResolver(schema)
+  });
 
-  const formSubmitHandler:SubmitHandler<FormInputs> =(data:FormInputs)=>{
-    console.log(data)
-  }
+  const formSubmitHandler: SubmitHandler<FormInputs> = (data: FormInputs) => {
+    console.log(data);
+  };
   return (
     <>
       <HeaderContainer>
